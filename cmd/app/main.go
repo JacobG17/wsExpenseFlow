798ec7a4a2ee2b3b5b760c3e4f64dd7672604bd4
Jacobo Gonzalez <jacobglez17@gmail.com>
2025-03-19 21:25:13 -0700

    Se agrega logica para poder cargar las credenciales desde variables de entorno

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"Api/wsExpenseFlow/internal/database"
 	"Api/wsExpenseFlow/internal/routes"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Cargar variables de entorno
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error al cargar el archivo .env")
+	}
+
 	// Abrir conexiones de base de datos
 	database.OpenConection()
 
-	//defer database.CloseDatabase()
+	defer database.CloseDatabase()
 
 	// Usar el router configurado en SetUpRouter()
 	r := routes.SetUpRouter()
